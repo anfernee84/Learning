74af5fcf37f2c207dc7c81c386639b5f90bd56ad
anfernee84 <eduard.rogulin@outlook.com>
2020-02-15 08:54:27 +0200

    add new files

---
+++ python/python.py
@@ -0,0 +1,7 @@
+num1 = float(input('Enter first digit '))
+num2 = float(input('Enter second digit '))
+sum = (num1++num2)
+print ('Result is', sum)
+res = input('Enter something: ')
+res *=5
+print (res)
