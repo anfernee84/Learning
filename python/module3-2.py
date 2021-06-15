d93cc9a8aa475839b480922b162a393fc35d0906
anfernee84 <eduard.rogulin@outlook.com>
2021-06-15 23:24:49 +0300

    update

--- python/module3-2.py
+++ python/module3-2.py
@@ -60,12 +60,75 @@ d2 = collections.defaultdict(int)
 for i in s: 
     d2[i] += 1
 print(dict(d2))
-'''
 
-a = {1:2, 3:4}
+
+a = {1:2, 3:4, 5:6}
 d = a
 for i in a.copy():
     if i == 1:
-        d.pop
+        a.pop(1)
 print(a)
+print(d)
+a.update ({9:9})
+
+a = [1,2, 3,4]
+d = a
+for i in a.copy():
+    if i == 1:
+        a.pop(1)
+print(a)
+print(d)
+'''
+'''
+print(a.popitem())
+
+keys = [1,2,3]
+values = [4,5,6]
+#d = {l1[0]:l2[0],l1[1]:l2[1],l1[2]:l2[2]}
+c = {}
+for i,v in enumerate(keys):
+    c[v] = values[i]
+print(c)
+'''
+
+'''
+d = {'math': 4, 'ukr': 5, 'phys' : 3, 'bio' : 4, 'geo' : 3}
+marks = {1:0,2:0,3:0,4:0,5:0}
+med = 0
+c = 0
+maxim = 0
+for i in d.values():
+    marks[i] += 1
+print(marks)
+
+
+for i in d.values():
+    if i > maxim:
+        maxim = i
+    else:
+        continue
+print (maxim)
+
+print(sum(d.values())/len(d))
+print(max(d.values()))
+
+marks = {i : 0 for i in range(1,6)}
+print(marks)
+
+
+keys = [1,2,3]
+values = [4,5,6]
+print(dict(zip(keys, values)))
+'''
+
+
+s = {1,2,3}
+l = [1,5,6,7,4,3,1,7,8,3,2,]
+print(set(l))
+#s = frozenset()
+s2 = {1,4,5}
+print(s&s2)
+print(s^s2)
+print(s|s2)
+print(s - s2)
 
